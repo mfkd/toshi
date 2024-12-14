d1e74f3b6d4cf7944a7a954edabb0efe872a1b42
mfkd <mattkfd@gmail.com>
2024-12-14 21:52:35 +0100

    fix: use selected book for download links
    Updated `FetchDownloadLinks` to use the selected book
    instead of defaulting to the first in the list. Ensures
    downloads align with user choice.

--- cmd/toshi/main.go
+++ cmd/toshi/main.go
@@ -46,8 +46,7 @@ func main() {
 	selectedBook := ui.SelectBook(libgen.FilterEPUB(books))
 	fmt.Println(selectedBook)
 
-	// TODO: Use selected book
-	downloadLinks := libgen.FetchDownloadLinks(c, books[0])
+	downloadLinks := libgen.FetchDownloadLinks(c, *selectedBook)
 	if err := libgen.TryDownloadLinks(c, downloadLinks, libgen.FileName(books[0])); err != nil {
 		log.Printf("Failed to download file for book %s: %v", books[0].Title, err)
 	}
