e855f5bae13dc788036ad9335fff6ac5e5b829b0
mfkd <mattkfd@gmail.com>
2024-12-25 18:30:36 +0100

    refactor: remove unused titleSearchURL function

--- internal/libgen/search.go
+++ internal/libgen/search.go
@@ -8,14 +8,6 @@ import (
 // Base URL for the LibGen search
 const libgenSearchBaseURL = "https://libgen.is/search.php"
 
-// Helper function to construct the search URL on TITLE Column
-func titleSearchURL(term string) string {
-	params := url.Values{}
-	params.Add("req", term)
-	params.Add("column", "title")
-	return fmt.Sprintf("%s?%s", libgenSearchBaseURL, params.Encode())
-}
-
 // Helper function to construct the search URL on ALL Column
 func defaultSearchURL(term string) string {
 	params := url.Values{}
